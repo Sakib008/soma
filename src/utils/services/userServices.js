6513c2d9220e5cec37e75b54c4bb4416a096fd33
Sakib008 <mdshakib1902@gmail.com>
2025-06-23 21:53:56 +0530

    feat : user service

---
+++ src/utils/services/userServices.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+const allUsers = async ()=>{
+    return await axios.get('/api/users')
+}
+
+const userGet =async (userId)=>{
+    try {
+        return await axios.get(`/api/users/${userId}`)
+    } catch (error) {
+        console.error(error.message)
+        throw error
+    }
+}
+
+const userEdit = async(userData)=>{
+    try {
+        return await axios.post(`/api/users/edit`,userData)
+    } catch (error) {
+        console.error(error.message)
+        throw error
+    }
+}
+
+const allUserBookmark = async ()=>{
+    try {
+        return await axios.get('/api/users/bookmark')
+    } catch (error) {
+        console.error(error.message)
+        throw error
+    }
+}
+
+const bookmarkPost =async (postId)=>{
+    try {
+        return await axios.post(`/api/users/bookmark/${postId}`)
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+}
+
+const removePost = async(postId)=>{
+    try {
+        return await axios.post(`/api/users/remove-bookmark/${postId}`)
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+}
+
+const postFollow =async (followUserId)=>{
+    try {
+        return await axios.post(`/api/users/follow/${followUserId}`)
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+}
+
+const postUnfollow = async (followUserId) =>{
+    try {
+        return await axios.post(`/api/users/unfollow/${followUserId}`)
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+}
+
+
+export {allUsers,userGet,userEdit,allUserBookmark,bookmarkPost,removePost,postFollow,postUnfollow}
