e26fa191c85c818ec2a4d0ea2abc9fb267b51105
Sakib008 <mdshakib1902@gmail.com>
2025-06-23 21:54:19 +0530

    feat  : combination of all service

---
+++ src/utils/services/index.js
@@ -0,0 +1,5 @@
+export {allPosts,singlePost,userPosts,postCreate,postEdit,postLike,postDislike,postDelete} from './postServices'
+
+export {allUsers,userGet,userEdit,allUserBookmark,bookmarkPost,removePost,postFollow,postUnfollow} from './userServices'
+
+export {login,signup} from './authService'
