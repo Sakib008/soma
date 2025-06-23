e6f5d7bcc951050eca75b261b1e684e7488bc778
Sakib008 <mdshakib1902@gmail.com>
2025-06-23 21:53:34 +0530

    fix : auth context

---
+++ src/Context/AuthContext.js
@@ -0,0 +1,30 @@
+import { createContext, useContext } from "react";
+import { login, signup } from "../utils/services/index";
+
+export const AuthContext = createContext();
+export function AuthProvider({ children }) {
+  const Token = JSON.parse(localStorage.getItem("token")) ?? "";
+
+  const loginHandler = async (username, password) => {
+    try {
+      const {
+        data: { foundUser, encodedToken },
+        status,
+      } = await login({username, password});
+      if (status === 200 || status === 201) {
+        localStorage.setItem("token", JSON.stringify(encodedToken));
+        localStorage.setItem("user", JSON.stringify(foundUser));
+      }
+    } catch (error) {
+      console.error("Error in Login Handle",error.message);
+      throw error;
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ Token, loginHandler }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+export const useAuth =()=>useContext(AuthContext)
