b50af24b6d73ee5ea5f8323a0490ac6fbcda5f1e
Sakib008 <mdshakib1902@gmail.com>
2025-06-25 22:15:13 +0530

    feat : backend post and user

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -5,31 +5,199 @@ import { formatDate } from "../utils/authUtils";
  * Posts can be added here.
  * You can add default posts of your wish with different attributes
  * */
+const postsContent = [
+  "📅 Not tomorrow. Not next week.\n✅ Start now, even if it's messy.\nSmall steps >>> No steps.",
+
+  "⚠️ Harsh truth: If you don’t take control, someone else will.\n✊ Be the hero of your own story.",
+
+  "⌛ You get 24 hours, just like everyone else.\n🧠 Protect your attention like your life depends on it—because it does.",
+
+  "🙅‍♂️ No to energy vampires\n🙅‍♀️ No to distractions\n✅ Yes to peace\n✅ Yes to priorities",
+
+  "👀 Don’t post every move.\n💪 Grind in private.\n🏆 Shine in public.",
+
+  "If you think you can’t…\nYou’re right.\nIf you think you can…\nYou’re also right.\n🧠 Upgrade your thoughts.",
+
+  "🔥 Go all in — but don’t rush.\n🌱 Everything good takes time.\nTrust the compound effect.",
+
+  "⛔ Endless content won’t change your life.\n✅ Execution will.\nToday, build something — even if it’s tiny.",
+
+  "👂 They don’t listen when you speak.\n💰 They ask how when you succeed.\nDon’t explain. Just execute.",
+
+  "Your vibe is your superpower.\n⚡ Protect it. Recharge it.\nDon’t let low-energy people drain your fire.",
+];
 
 export const posts = [
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: postsContent[0],
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
+    comment: [
+      {
+        _id: uuid(),
+        username: "janedoe",
+        text: "I agree!! to manage what yay?",
+      },
+    ],
     username: "adarshbalika",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
   {
     _id: uuid(),
-    content:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+    content: postsContent[2],
     likes: {
       likeCount: 0,
       likedBy: [],
       dislikedBy: [],
     },
-    username: "shubhamsoni",
+    comment: [
+      {
+        _id: uuid(),
+        username: "janedoe",
+        text: "I agree!!  go for outing. What say?",
+      },
+    ],
+    username: "adarshbalika",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: postsContent[1],
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comment: [
+      { _id: uuid(), username: "janedoe", text: "I like  outing. What say?" },
+    ],
+    username: "shubham",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: postsContent[3],
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comment: [
+      {
+        _id: uuid(),
+        username: "janedoe",
+        text: "I agree!! Feels like . What say?",
+      },
+    ],
+    username: "shubham",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: postsContent[4],
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comment: [
+      {
+        _id: uuid(),
+        username: "janedoe",
+        text: "I do listen this , What say?",
+      },
+    ],
+    username: "sameer001",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: postsContent[5],
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comment: [
+      { _id: uuid(), username: "janedoe", text: "I manage my day with this.?" },
+    ],
+    username: "sameer001",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: postsContent[6],
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comment: [
+      { _id: uuid(), username: "janedoe", text: "Very Best outing. What say?" },
+    ],
+    username: "saziya243",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: postsContent[7],
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+
+    comment: [{ _id: uuid(), username: "janedoe", text: "I agree! What say?" }],
+    username: "saziya243",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: postsContent[8],
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comment: [
+      {
+        _id: uuid(),
+        username: "janedoe",
+        text: " Feels like we should go for outing.",
+      },
+    ],
+    username: "piyush",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    content: postsContent[9],
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    },
+    comment: [
+      {
+        _id: uuid(),
+        username: "janedoe",
+        text: "I agree!! I will meet. What say?",
+      },
+    ],
+    username: "sakib",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -1,5 +1,5 @@
 import { v4 as uuid } from "uuid";
-import { formatDate } from "../utils/authUtils";
+import { formatDate } from "../utils/authUtils.js";
 /**
  * User Database can be added here.
  * You can add default users of your wish with different attributes
@@ -8,10 +8,56 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
+    firstName: "Mohammad",
+    lastName: "Sakib",
+    username: "sakib",
+    password: "Sakib#123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: uuid(),
     firstName: "Adarsh",
     lastName: "Balika",
-    username: "sakib",
-    password: "sakib123",
+    username: "adarshbalika",
+    password: "adarshBalika@123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Saziya",
+    lastName: "Parveen",
+    username: "saziya243",
+    password: "Saziya@293",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Piyush",
+    lastName: "Ranjan",
+    username: "piyush",
+    password: "Piyush@999",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Shubham",
+    lastName: "Sharma",
+    username: "shubham",
+    password: "Shubham@959",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Mohammad",
+    lastName: "Sameer",
+    username: "sameer001",
+    password: "Mohammad@123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
