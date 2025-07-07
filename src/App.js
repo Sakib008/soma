5ad68ef9df1c1c61a673770bc7f997ce37042601
Sakib008 <mdshakib1902@gmail.com>
2025-07-07 20:27:18 +0530

    feat : handle auth on the app main page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import './App.css'
-import Login from './components/auth/Login';
-import Header from './components/Header'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/auth/Login";
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import { useAuth } from "./Context/AuthContext";
+import Explore from "./Pages/Explore/Explore";
+import SinglePost from "./Pages/SinglePost/SinglePost.jsx";
 
 function App() {
-  const auth = false;
+  const { Token } = useAuth();
   return (
     <>
-    {!auth ? <div className=''>
-      <Login/>
-    </div> : 
-   <div className=''>
-    <Header/>
-   </div>
-   } 
+      {!Token ? (
+        <div className="">
+          <Login />
+        </div>
+      ) : (
+        <div className="">
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  
+                </Layout>
+              }
+            />
+            <Route
+              path="/explore"
+              element={
+                <Layout>
+                  <Explore />
+                </Layout>
+              }
+            />
+            <Route
+              path={`/post/:postId`}
+              element={
+                <Layout>
+                  <SinglePost/>
+                </Layout>
+              }
+            />
+          </Routes>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
