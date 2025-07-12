58dbeb0145cad68a8dccf6c4c194bb4e9da666e6
Sakib008 <mdshakib1902@gmail.com>
2025-07-12 13:31:04 +0530

    feat : api services to handle data from backend

--- src/utils/services/postServices.js
+++ src/utils/services/postServices.js
@@ -1,76 +1,93 @@
 import axios from "axios";
 
-const allPosts = async ()=>{
-    try {
-        return await axios.get(`/api/posts`)
-    } catch (error) {
-        console.error(error.message)
-        throw error
-    }
-}
+const allPosts = async () => {
+  try {
+    return await axios.get(`/api/posts`);
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
 
-const singlePost = async (postId)=>{
-    try {
-        return await axios.get(`/api/posts/${postId}`)
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-}
+const singlePost = async (postId) => {
+  try {
+    return await axios.get(`/api/posts/${postId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-const userPosts = async (username)=>{
-    try {
-        return await axios.get(`/api/posts/user/${username}`)
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-}
+const userPosts = async (username) => {
+  try {
+    return await axios.get(`/api/posts/user/${username}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-const postCreate = async (content)=>{
-    try {
-        return await axios.post(`/api/user/posts`,content)
-    } catch (error) {
-        console.error(error)
-        throw error
+const postCreate = async (encodedToken,content) => {
+  try {
+    return await axios.post(`/api/posts`, {content : content},{
+      headers : {
+        authorization : encodedToken
+      }
+    });
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
 
-    }
-}
+const postEdit = async (postId, postData) => {
+  try {
+    return await axios.post(`/api/posts/edit/${postId}`, postData);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-const postEdit = async (postId,postData)=>{
-    try {
-        return await axios.post(`/api/posts/edit/${postId}`,postData)
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-}
+const postLike = async (postId,encodedToken) => {
+  try {
+    return await axios.post(`/api/posts/like/${postId}`,{}, {
+      headers: { authorization : JSON.stringify(encodedToken) },
+    });
+   
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-const postLike = async (postId)=>{
-    try {
-        return await axios.post(`/api/posts/like/${postId}`)
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-}
+const postDislike = async (postId,encodedToken) => {
+  try {
+    return await axios.post(`/api/posts/dislike/${postId}`,{},{
+      headers: { authorization : JSON.stringify(encodedToken) }
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-const postDislike = async (postId)=>{
-    try {
-        return await axios.post(`/api/posts/dislike/${postId}`)
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-}
+const postDelete = async (postId) => {
+  try {
+    return await axios.post(`/api/user/posts/${postId}`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-const postDelete = async (postId)=>{
-    try {
-        return await axios.post(`/api/user/posts/${postId}`)
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-}
-
-export {allPosts,singlePost,userPosts,postCreate,postEdit,postLike,postDelete,postDislike}
+export {
+  allPosts,
+  singlePost,
+  userPosts,
+  postCreate,
+  postEdit,
+  postLike,
+  postDelete,
+  postDislike,
+};
