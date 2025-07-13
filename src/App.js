c9bc66ee77ec56a5f0e9305b03c28572fa9dee43
Sakib008 <mdshakib1902@gmail.com>
2025-07-13 13:13:25 +0530

    fix : the layout page

--- src/App.js
+++ src/App.js
@@ -1,52 +1,66 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/auth/Login";
-import Header from "./components/Header";
 import Layout from "./components/Layout";
 import { useAuth } from "./Context/AuthContext";
 import Explore from "./Pages/Explore/Explore";
 import SinglePost from "./Pages/SinglePost/SinglePost.jsx";
+import Signup from "./components/auth/Signup.jsx";
+import Like from "./Pages/Likes/Like.jsx";
+import Bookmark from "./Pages/Bookmark/Bookmark.jsx";
+import Home from "./Pages/Home/Home.jsx";
 
 function App() {
   const { Token } = useAuth();
   return (
-    <>
-      {!Token ? (
-        <div className="">
-          <Login />
-        </div>
-      ) : (
-        <div className="">
-          <Header />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Layout>
-                  
-                </Layout>
-              }
-            />
-            <Route
-              path="/explore"
-              element={
-                <Layout>
-                  <Explore />
-                </Layout>
-              }
-            />
-            <Route
-              path={`/post/:postId`}
-              element={
-                <Layout>
-                  <SinglePost/>
-                </Layout>
-              }
-            />
-          </Routes>
-        </div>
-      )}
-    </>
+    <div className="">
+      <Routes>
+        {/* Public Route */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* Private Route */}
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/explore"
+          element={
+            <Layout>
+              <Explore />
+            </Layout>
+          }
+        />
+        <Route
+          path={`/post/:postId`}
+          element={
+            <Layout>
+              <SinglePost />
+            </Layout>
+          }
+        />
+        <Route
+          path="/like"
+          element={
+            <Layout>
+              <Like />
+            </Layout>
+          }
+        />
+        <Route
+          path="/bookmark"
+          element={
+            <Layout>
+              <Bookmark />
+            </Layout>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
