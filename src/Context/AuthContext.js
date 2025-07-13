26cec8f9ec91efb0658b539eba707011555cb35f
Sakib008 <mdshakib1902@gmail.com>
2025-07-13 13:17:15 +0530

    fix : updated with signup function

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -4,36 +4,40 @@ import { login, signup } from "../utils/services/index";
 export const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const Token = JSON.parse(localStorage.getItem("token")) ?? "";
-  const User = JSON.parse(localStorage.getItem("user")??'')
+  const User = JSON.parse(localStorage.getItem("user")) ?? "";
 
   const loginHandler = async (username, password) => {
     try {
       const {
         data: { foundUser, encodedToken },
         status,
-      } = await login({username, password});
+      } = await login({ username, password });
       if (status === 200 || status === 201) {
         localStorage.setItem("token", JSON.stringify(encodedToken));
         localStorage.setItem("user", JSON.stringify(foundUser));
       }
     } catch (error) {
-      console.error("Error in Login Handle",error.message);
+      console.error("Error in Login Handle", error.message);
       throw error;
     }
   };
 
-  const usersHandler = async()=>{
+  const signupUser = async (userdata) => {
     try {
-      
+      const { data, status } = await signup(userdata);
+      if (status === 200 || status === 201) {
+        console.log("Data of the user : ", data);
+      }
     } catch (error) {
-      
+      console.error("Error : ", error.message);
+      throw error;
     }
-  }
+  };
 
   return (
-    <AuthContext.Provider value={{ Token, loginHandler,User }}>
+    <AuthContext.Provider value={{ Token, loginHandler, User, signupUser }}>
       {children}
     </AuthContext.Provider>
   );
 }
-export const useAuth =()=>useContext(AuthContext)
+export const useAuth = () => useContext(AuthContext);
