1b1c80834930af0b93cb3de591e2bdc4fd823e2a
Sakib008 <mdshakib1902@gmail.com>
2025-07-13 13:16:37 +0530

    fix : sidebar function added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { BiBookmark, BiHome } from "react-icons/bi";
-import { BsHeart, BsPerson } from "react-icons/bs";
+import { BsHeart} from "react-icons/bs";
 import { RiRocket2Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
@@ -8,14 +8,13 @@ const Sidebar = ({ onOpenModal }) => {
     { name: "home", path: "/", icon: BiHome },
     { name: "explore", path: "/explore", icon: RiRocket2Line },
     { name: "bookmark", path: "/bookmark", icon: BiBookmark },
-    { name: "profile", path: "/profile", icon: BsPerson },
     { name: "likes", path: "/like", icon: BsHeart },
   ];
   return (
-    <div className="flex flex-col h-screen fixed bg-secondary-bg text-2xl w-80 text-primary-text justify-start">
+    <div className="flex flex-col h-full bg-secondary-bg text-2xl w-80 text-primary-text justify-start">
       {sideLink.map(({ name, path, icon: Icon }, index) => (
         <Link
-          className="ml-6 my-1 flex items-center p-2 gap-2"
+          className="ml-6 my-1 flex items-center p-2 gap-2 hover:bg-primary-bg/20 transition-colors"
           key={index}
           to={path}
         >
@@ -24,7 +23,7 @@ const Sidebar = ({ onOpenModal }) => {
         </Link>
       ))}
       <button
-        className="bg-primary-text text-secondary-bg my-6 p-2 w-4/5 mx-auto font-bold"
+        className="bg-primary-text text-secondary-bg my-6 p-2 w-4/5 mx-auto font-bold hover:bg-primary-text/90 transition-colors"
         onClick={onOpenModal}
       >
         Create a Post
