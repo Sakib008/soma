900b3d0002be4d028a0a6c59c15d271e4bad31cf
Sakib008 <mdshakib1902@gmail.com>
2025-07-13 11:46:25 +0530

    fix : create post popup fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,28 @@
+import Header from "./Header"
 import Sidebar from "./Sidebar"
+import CreatePostModal from "./CreatePostModal"
+import { useAuth } from "../Context/AuthContext"
+import { useState } from "react"
 
 const Layout = ({children}) => {
+  const { User } = useAuth();
+  const [showModal, setShowModal] = useState(false);
+  
   return (
-    <div className="flex h-screen pt-16"> 
-      <Sidebar />
-      <main className="flex-1 overflow-auto">
-        <div className="flex justify-center pt-6 h-full">
-          {children}
-        </div>
-      </main>
-    </div>
+    <>
+      <div className="sticky top-0 z-10 w-full">
+        <Header/>
+      </div>
+      <div className="flex h-[calc(100vh-4rem)] pt-0"> 
+        <Sidebar onOpenModal={() => setShowModal(true)} />
+        <main className="flex-1 overflow-auto ml-80">
+          <div className="flex justify-center pt-6 h-full">
+            {children}
+          </div>
+        </main>
+      </div>
+      {showModal && <CreatePostModal user={User} onClose={() => setShowModal(false)} />}
+    </>
   )
 }
 
