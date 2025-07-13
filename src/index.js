c02f5bf9f9124d81cb927863eb137feff07a923c
Sakib008 <mdshakib1902@gmail.com>
2025-07-13 16:23:55 +0530

    fix : post context

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { makeServer } from "./server";
 import { AuthProvider } from "./Context/AuthContext";
+import { BrowserRouter as Router } from "react-router-dom";
+import { PostProvider } from "./Context/PostContext";
 
 makeServer();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PostProvider>
+      <Router>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Router>
+    </PostProvider>
   </AuthProvider>
 );
 
