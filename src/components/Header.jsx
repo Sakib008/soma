ef280cd4eb9e8c3589355ec0f0d36d97a5d135b3
Sakib008 <mdshakib1902@gmail.com>
2025-07-14 22:11:58 +0530

    fix : added the profile layout at header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { TbMessageForward } from "react-icons/tb";
+import { usePost } from "../Context/PostContext";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const {state} = usePost(); 
   return (
     <div className="bg-secondary-bg p-2 top-0 w-full flex h-16 justify-between items-center shadow-md">
         <div className='logo mx-8'>
@@ -9,9 +12,9 @@ const Header = () => {
             SOMA
           </h1>
         </div>
-        <div className="auth text-2xl flex items-center cursor-pointer p-1 font-semibold text-primary-text mx-4 border-4 border-secondary-text px-2 rounded-3xl hover:bg-primary-text hover:text-secondary-bg transition-colors"> 
-          Logout 
-        </div>
+        <Link to={'/profile'} className="text-xl size-14 rounded-full cursor-pointer p-1 font-semibold text-primary-text  border-secondary-text hover:bg-primary-text hover:text-secondary-bg transition-colors"> 
+          <img className="rounded-full w-full h-full" src={state.profile.avatar || `https://ui-avatars.com/api/?name=${state.profile.username}&length=1`} alt={state.profile.username}/> 
+        </Link>
     </div>
   )
 }
