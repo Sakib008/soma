ff6aaa6977218f00729196e0656724fb08b8e205
Sakib008 <mdshakib1902@gmail.com>
2025-07-14 22:15:11 +0530

    feat : added user avatar of image

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,17 +4,17 @@ const User = ({user}) => {
   return (
     <div className="flex items-center gap-3 p-2">
       <div>
-        {user.avatar ? (
+        {/* {user.avatar ? ( */}
           <img
-            className="w-12 h-12 rounded-full object-cover"
-            src={user.avatar}
+            className="w-12 h-12 rounded-full object-cover flex items-center justify-center text-xl font-bold"
+            src={user.avatar || `https://ui-avatars.com/api/?name=${user.username}&length=1`}
             alt={user.username}
           />
-        ) : (
-          <div className="w-12 h-12 rounded-full bg-gray-300 flex items-center justify-center text-xl font-bold">
-            {user.username?.charAt(0).toUpperCase()}
-          </div>
-        )}
+        {/* // ) : (
+        //   <div className="w-12 h-12 rounded-full bg-gray-300 flex items-center justify-center text-xl font-bold">
+        //     {user.username?.charAt(0).toUpperCase()}
+        //   </div>
+        // )} */}
       </div>
       <div className="flex flex-col text-xl">
         <span className="font-semibold">@{user.username}</span>
