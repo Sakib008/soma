439401d2cd1e4f696ec5833d83a2b348f9bcd200
Sakib008 <mdshakib1902@gmail.com>
2025-07-22 19:43:46 +0530

    fix : imported loading for better work

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
-
 const ProtectedRoute = ({ children }) => {
   const location = useLocation();
-  const { Token } = useAuth();
-
+  const { Token,loading } = useAuth();
+  console.log("Token in ProtectedRoute:", Token);
+  if(loading) {
+    return <div className="text-center text-xl font-semibold">Loading...</div>; 
+  };    
   if (!Token) {
     return <Navigate to={"/login"} state={{ from: location }} replace />;
   }
