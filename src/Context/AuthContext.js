4986e414281a0bbd877d79f7d2d763afe855e4c5
Sakib008 <mdshakib1902@gmail.com>
2025-07-22 19:43:12 +0530

    fix : user and loading for fetching

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,11 +1,20 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useEffect, } from "react";
 import { login, signup } from "../utils/services/index";
 
 export const AuthContext = createContext();
 export function AuthProvider({ children }) {
-  const Token = JSON.parse(localStorage.getItem("token")) ?? "";
-  const User = JSON.parse(localStorage.getItem("user")) ?? "";
-
+  // Use state for token and user
+  const [Token, setToken] = useState("");
+  const [User, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // On mount, initialize from localStorage
+  useEffect(() => {
+    const storedToken = JSON.parse(localStorage.getItem("token"));
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    setToken(storedToken ?? '');
+    setUser(storedUser ?? null);
+    setLoading(false);
+  }, []);
   const loginHandler = async (username, password) => {
     try {
       const {
@@ -15,6 +24,9 @@ export function AuthProvider({ children }) {
       if (status === 200 || status === 201) {
         localStorage.setItem("token", JSON.stringify(encodedToken));
         localStorage.setItem("user", JSON.stringify(foundUser));
+        setToken(encodedToken);
+        setUser(foundUser);
+        console.log("Login successful, user set in context");
       }
     } catch (error) {
       console.error("Error in Login Handle", error.message);
@@ -24,9 +36,13 @@ export function AuthProvider({ children }) {
 
   const signupUser = async (userdata) => {
     try {
-      const { data, status } = await signup(userdata);
+      const { data: { createdUser, encodedToken }, status } = await signup(userdata);
       if (status === 200 || status === 201) {
-        console.log("Data of the user : ", data);
+        localStorage.setItem("token", JSON.stringify(encodedToken));
+        localStorage.setItem("user", JSON.stringify(createdUser));
+        setToken(encodedToken);
+        setUser(createdUser);
+        
       }
     } catch (error) {
       console.error("Error : ", error.message);
@@ -34,8 +50,16 @@ export function AuthProvider({ children }) {
     }
   };
 
+  const logoutUser = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setToken("");
+    setUser(null);
+    console.log("All Auth deleted");
+  };
+
   return (
-    <AuthContext.Provider value={{ Token, loginHandler, User, signupUser }}>
+    <AuthContext.Provider value={{ Token, loginHandler,loading, User, signupUser, logoutUser }}>
       {children}
     </AuthContext.Provider>
   );
