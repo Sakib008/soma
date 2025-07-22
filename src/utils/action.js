747293ec1b6b2d82b242a41e8a2e9a593eafbfa9
Sakib008 <mdshakib1902@gmail.com>
2025-07-22 19:42:50 +0530

    fix : NAME COMMENT

--- src/utils/action.js
+++ src/utils/action.js
@@ -1,3 +1,4 @@
+// Posts Action Types
 const GET_ALL_POSTS = 'post/getAll';
 const GET_SUGGESSTED_POSTS = 'post/suggested';
 const GET_BOOKMARK_POSTS = 'post/bookmarked';
@@ -7,12 +8,16 @@ const LIKE_POST = 'post/like';
 const BOOKMARK_POST = 'post/bookmark'
 const EDIT_POSTS = 'post/edit';
 const DELETE_POSTS = 'post/delete';
+
+// User Action Types
 const ADD_USER_AVATAR = 'profile/avatar';
 const ADD_USER_BIO = 'profile/bio';
 const ADD_USER_PORTFOLIO = 'profile/portfolio';
 const FOLLOW_USER ='user/follow';
 const UNFOLLOW_USER = 'user/unfollow';
 const USER_PROFILE = 'user/profile';
+
+// Getting User Data and His Posts
 const GET_ALL_USERS = 'user/getAll';
 const GET_USER_POSTS = 'user/posts';
 const ADD_BOOKMARK = 'posts/add-bookmark';
