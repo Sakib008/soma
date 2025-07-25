57b760702ef080683698ab2fd71e818b6540b400
Sakib008 <mdshakib1902@gmail.com>
2025-07-25 12:50:22 +0530

    feat : completed footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-gray-800 text-white p-4 text-center min-h-[100px] ">
+        <p>© 2023 My Soma. All rights reserved.</p>
+        <p>Follow us on <a href="https://twitter.com/mysoma">Twitter</a> and <a href="linkedin.com/company/mysoma">LinkedIn</a>.</p>
+        <p>Contact us at <a href="mailto:mdshakib1902@gmail.com">gmail</a>.</p>
+        <p>Privacy Policy | Terms of Service</p>
+        <p>Made with ❤️ by Mohammad Sakib</p>
+    </div>
+  )
+}
+
+export default Footer
