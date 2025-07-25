f533743d37895648b6964e19f4ecb2a1da8e343d
Sakib008 <mdshakib1902@gmail.com>
2025-07-25 19:01:18 +0530

    fix : role base btn and responsive

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -8,21 +8,29 @@ const Profile = () => {
   const { state, getAllUser, getAllPost } = usePost();
   const { logoutUser, loading } = useAuth();
   const { username } = useParams();
-  const user =
+  const owner = state.profile.username === username;
+  const user = owner ?
+    state.profile :
     state.users.all.find((user) => user.username === username) ||
     {};
 
+    console.log("user", !user.username ? "No user found" : user);
+    console.log("posts", state.posts.all);
   useEffect(() => {
    const fetchUserData = async () => {
-      if (!user.username) {
-        await getAllUser;
+     if (!user.username) {
+        await getAllUser(username);
       }
+      if (!state.profile.username) {
+        await getAllUser(username);
+      }
+      // Fetch all posts if not already fetched
       if (state.posts.all.length === 0) {
         await getAllPost();
       }
     }
     fetchUserData();
-  }, [user.username, getAllUser, getAllPost, state.posts.all.length]);
+  }, [user.username,state.profile.username, state.posts.all.length]);
 
   if (loading) {
     return <div className="text-center text-xl font-semibold">Loading...</div>;
@@ -37,7 +45,7 @@ const Profile = () => {
   return (
     <div className="flex flex-col items-center gap-8 max-w-3xl mx-auto py-8">
       {/* Profile Header */}
-      <div className="w-full bg-white rounded-3xl shadow-lg p-8 flex flex-col items-center gap-4">
+      <div className="w-full bg-white rounded-3xl shadow-lg p-8 flex flex-col justify-center items-center gap-4">
         <div className="flex items-center gap-6">
           <img
             className="w-24 h-24 rounded-full object-cover border-4 border-blue-300"
@@ -79,12 +87,12 @@ const Profile = () => {
             <span className="font-bold">{following.length}</span> Following
           </div>
         </div>
-        <button
+       {owner && <button
           onClick={logoutUser}
           className="text-xl text-white font-semibold rounded-2xl p-2 bg-secondary-bg mt-4"
         >
           Log out
-        </button>
+        </button>}
       </div>
 
       {/* User's Posts */}
